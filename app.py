47856bb8a07e1594bd40403300c1575e39ed3c96
KoteTheInnkeeper <lucianocorrea58@gmail.com>
2021-06-10 19:06:33 -0300

    Adding the coefficients.

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from utils.combustibles import Combustible
+from lib.coefficients import Coefficients
 
-lignite_coal = Combustible("Lignite coal", [51.12, 3.89, 14.65, 0.61, 1.87, 14.36, 13.5])
+lignite_coal = ["Lignite coal", [51.12, 3.89, 14.65, 0.61, 1.87, 14.36, 13.5]]
+natural_gas = ["Natural gas", [76, 23, 2, 1, 0, 0, 0]]
+
 
 print(lignite_coal)
